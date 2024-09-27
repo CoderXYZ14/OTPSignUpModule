c3fc426928a3ccc6a577dce3da8903b0e58d97f8
CoderXYZ14 <s1404islam@gmail.com>
2024-09-27 19:53:32 +0530

    App.js added

---
+++ server/src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.routes.js";
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cookieParser());
+
+app.use("/api/v1/users", userRouter);
+
+export { app };
