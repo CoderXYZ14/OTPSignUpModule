42640859fc13d5c6a8c3dc50a2ee8f7046f642e2
CoderXYZ14 <s1404islam@gmail.com>
2024-09-28 23:18:22 +0530

    Bug fix-5

--- client/src/components-page/signin/Signin.jsx
+++ client/src/components-page/signin/Signin.jsx
@@ -1,5 +1,4 @@
 import { Input } from "@/components/ui/input";
-
 import { Loader2 } from "lucide-react";
 import { useState } from "react";
 import {
@@ -60,8 +59,8 @@ const Signin = () => {
 
   return (
     <div className="bg-slate-300 h-screen flex justify-center items-center">
-      <div className="flex flex-col justify-center">
-        <div className="rounded-lg bg-white w-[500px] text-center p-2 h-max px-4">
+      <div className="flex flex-col justify-center w-full max-w-md">
+        <div className="rounded-lg bg-white shadow-lg p-6 mx-4">
           <Heading label={"Sign In"} />
           <SubHeading label={"Enter your info"} />
           <div className="flex items-center space-x-2 mt-4">
@@ -85,16 +84,18 @@ const Signin = () => {
               label="phone"
               className="w-full text-sm"
             />
-            {errorMessage && (
-              <p className="text-red-500 text-sm mt-2">{errorMessage}</p> // Display error message
-            )}
-            <div className="pt-4">
-              <div className="flex justify-end gap-5">
-                <Button disabled={!phone || loading} onClick={requestOtp}>
-                  {loading ? <Loader2 className="animate-spin" /> : "Create"}
-                </Button>
-              </div>
-            </div>
+          </div>
+          {errorMessage && (
+            <p className="text-red-500 text-sm mt-2">{errorMessage}</p> // Display error message
+          )}
+          <div className="pt-4">
+            <Button
+              disabled={!phone || loading}
+              onClick={requestOtp}
+              className="w-full"
+            >
+              {loading ? <Loader2 className="animate-spin" /> : "Create"}
+            </Button>
           </div>
         </div>
       </div>
